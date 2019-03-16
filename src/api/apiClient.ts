a2e7a8b5d2c95c5917a3932b462074fb8d27b35b
yvln <sayyveline@gmail.com>
2019-03-16 19:17:52 +0100

    feat: Create fake API

---
+++ src/api/apiClient.ts
@@ -0,0 +1,44 @@
+import sha1 from 'sha1';
+
+import messagesFixtures from './messages.fixtures.json';
+import { IMessage } from '../types';
+
+const messages = messagesFixtures;
+
+function getMessages(): Promise<IMessage[]> {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve(messages.data);
+    }, 1000);
+  });
+}
+
+function getUser(): Promise<{ name: string }> {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve({ name: 'yvln' });
+    }, 300);
+  });
+}
+
+function postMessage(
+  message: string,
+  isPrivate: boolean,
+  username: string
+): Promise<any> {
+  messages.data.unshift({
+    message,
+    private: isPrivate,
+    username,
+    date: new Date().toString(),
+    id: sha1(new Date().toString()).toString(),
+  });
+
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve();
+    }, 100);
+  });
+}
+
+export default { getMessages, getUser, postMessage };
