11638aecd4f51f4a4ac624c203f2a9e5a1e28153
yvln <sayyveline@gmail.com>
2019-03-17 17:07:48 +0100

    feat: Configure fake api to send errors

--- src/api/apiClient.ts
+++ src/api/apiClient.ts
@@ -3,15 +3,22 @@ import sha1 from 'sha1';
 import { IMessage } from '../types';
 import messagesFixtures from './messages.fixtures.json';
 
+// FAKE API, returns fake data with a delay
+
 const messages = messagesFixtures;
 const apiResponseDelay = 700;
 
-// FAKE API, returns fake data with a delay
+// set sendErrors to `true` if you want to test errors handling
+const sendErrors = false;
 
 function getMessages(): Promise<IMessage[]> {
   return new Promise((resolve, reject) => {
     setTimeout(() => {
-      resolve(messages.data);
+      if (!sendErrors) {
+        resolve(messages.data);
+      } else {
+        reject('Unable to fetch messages');
+      }
     }, apiResponseDelay);
   });
 }
@@ -29,19 +36,22 @@ function postMessage(
   isPrivate: boolean,
   username: string
 ): Promise<any> {
-  // Add the new data at the beginning so it is sorted by date
-  messages.data.unshift({
-    message,
-    private: isPrivate,
-    username,
-    date: new Date().toString(),
-    // use sha1 to get a unique hash per message
-    id: sha1(`${new Date().toString()}-${username}`).toString(),
-  });
-
   return new Promise((resolve, reject) => {
     setTimeout(() => {
-      resolve('success');
+      if (!sendErrors) {
+        // Add the new data at the beginning so it is sorted by date
+        messages.data.unshift({
+          message,
+          private: isPrivate,
+          username,
+          date: new Date().toString(),
+          // use sha1 to get a unique hash per message
+          id: sha1(`${new Date().toString()}-${username}`).toString(),
+        });
+        resolve('success');
+      } else {
+        reject('Unable to post the message');
+      }
     }, apiResponseDelay);
   });
 }
